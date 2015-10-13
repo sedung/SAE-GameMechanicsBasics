77540dfcbf1c26c8b4b809a175ccc53311fb7a1a
Robert K. Pauls <r.pauls@robertkpauls.com>
2015-10-13 15:07:54 +0200

    Added cubeMovement and cursorLock;

---
+++ source/IntroductionProject/cubeMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeMovement : MonoBehaviour {
+
+    public float speed = 1.2f;
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        this.transform.position += move * speed * Time.deltaTime ;
+
+        // goo.gl/yKvF3k
+    }
+}
---
+++ source/IntroductionProject/cursorLock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cursorLock : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+    }
+}
