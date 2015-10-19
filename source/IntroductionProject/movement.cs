983d8dcc0ffe3212effa6ef6d2d094bbe7f222d4
Robert K. Pauls <r.pauls@robertkpauls.com>
2015-10-19 17:05:05 +0200

    Aded physic based movement;

---
+++ source/IntroductionProject/movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeMovement : MonoBehaviour {
+
+    public float speedForward = 10.0f;
+    public float speedSideway = 7.5f;
+
+    void Update ()
+    {
+        // Roate object
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * 5 * Time.deltaTime, 0));
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Rigidbody rig = GetComponent<Rigidbody>();
+        // Move object based on added force
+        rig.AddForce(Vector3.forward * Input.GetAxis("Vertical") * speedForward, ForceMode.Acceleration);
+        rig.AddForce(Vector3.right * Input.GetAxis("Horizontal") * speedSideway);
+
+    }
+}
